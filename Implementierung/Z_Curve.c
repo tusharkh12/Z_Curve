897974e73b7d3b49b6c73c985fe8d327ecbd2174
Pascal <pascal39@gmx.net>
2023-06-15 16:40:17 +0200

    still drawing squares instead of z

---
+++ Implementierung/Z_Curve.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+
+typedef int coord_t;
+
+void z_curve(unsigned degree, coord_t* x, coord_t* y);
+
+void z_curve_at(unsigned degree, size_t idx, coord_t* x, coord_t* y);
+
+size_t z_curve_pos(unsigned degree, coord_t x, coord_t y);
+
+int main(int argc, char *argv[]) {
+    //for testing purpose
+    unsigned testDegree = 3;
+    //calculate number of points based on degree
+    unsigned numberOfPoints = 1 << (2 * testDegree);
+
+    //allocate space of coordinates
+    coord_t* x = (coord_t*)malloc(numberOfPoints * sizeof(coord_t));
+    coord_t* y = (coord_t*)malloc(numberOfPoints * sizeof(coord_t));
+
+    //create image (SVG for now)
+    FILE* svgFile = fopen("zcurve.svg", "w");
+    if (svgFile == NULL) {
+        printf("Error opening the SVG file.\n");
+        return -1;
+    }
+
+    //clock setup (not sure if ideal setup)
+    clock_t begin, end;
+    float z;
+    begin = clock();
+
+    z_curve(testDegree, x, y);
+
+    end = clock();
+    z = end - begin;
+    z /= CLOCKS_PER_SEC;
+    if (z != 0) {
+        printf("Runtime: %f seconds\n", z);
+    }
+
+    /*
+    printf("Z-Curve Coordinates:\n");
+    for (unsigned i = 0; i < numberOfPoints; i++) {
+        printf("(%d, %d)\n", x[i], y[i]);
+    }
+    */
+
+    //to get the width and height of the SVG Image we first find min/max x and y
+    coord_t scalingFactor = 10;
+    coord_t minX = x[0];
+    coord_t minY = y[0];
+    coord_t maxX = x[0];
+    coord_t maxY = y[0];
+    for (unsigned i = 1; i < numberOfPoints; i++) {
+        if (x[i] < minX) minX = x[i];
+        if (x[i] > maxX) maxX = x[i];
+        if (y[i] < minY) minY = y[i];
+        if (y[i] > maxY) maxY = y[i];
+    }
+
+    //dimensions of the SVG image
+    coord_t width = (maxX - minX + 1) * scalingFactor;
+    coord_t height = (maxY - minY + 1) * scalingFactor;
+
+    fprintf(svgFile, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\">\n", width, height);
+
+    for (unsigned i = 0; i < numberOfPoints; i++) {
+    // Adjust the coordinates by subtracting the minimum values
+    coord_t adjustedX1 = (x[i] - minX) * scalingFactor;
+    coord_t adjustedY1 = (y[i] - minY) * scalingFactor;
+
+    // Check if there is a next point to draw a line
+    if (i + 1 < numberOfPoints) {
+        coord_t adjustedX2 = (x[i + 1] - minX) * scalingFactor;
+        coord_t adjustedY2 = (y[i + 1] - minY) * scalingFactor;
+
+        fprintf(svgFile, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" />\n",
+            adjustedX1, adjustedY1, adjustedX2, adjustedY2);
+    }
+}
+
+    fprintf(svgFile, "</svg>"); //footer
+
+    //free allocated memory
+    free(x);
+    free(y);
+    fclose(svgFile);
+
+    return 0;
+}
+
+void z_curve(unsigned degree, coord_t* x, coord_t* y) {
+    unsigned size = 1 << degree; // Anzahl der Punkte auf der Z-Kurve
+    int index = 0; // Index für den Puffer
+
+    // Startkoordinaten
+    coord_t start_x = 0;
+    coord_t start_y = 0;
+    coord_t current_x = start_x;
+    coord_t current_y = start_y;
+    coord_t sign = 1;
+
+    x[index] = current_x;
+    y[index] = current_y;
+    index++;
+
+    for (unsigned i = 0; i < degree; i++) {
+        unsigned offset = 1 << i;
+
+        // Rechts oben
+        current_x = current_x - sign * offset;
+        x[index] = current_x;
+        y[index] = current_y;
+        index++;
+
+        // Links unten
+        current_y = current_y + sign * offset;
+        x[index] = current_x;
+        y[index] = current_y;
+        index++;
+
+        // Links oben
+        current_x = current_x + sign * offset;
+        x[index] = current_x;
+        y[index] = current_y;
+        index++;
+
+        // Rechts unten
+        current_y = current_y - sign * offset;
+        x[index] = current_x;
+        y[index] = current_y;
+        index++;
+
+        sign = -sign;
+    }
+}
