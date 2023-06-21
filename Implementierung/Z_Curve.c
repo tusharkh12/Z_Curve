5adf90a94ca4d3ebd54ac6d6cc519da068b07380
Pascal <pascal39@gmx.net>
2023-06-21 19:31:17 +0200

    moved methods after main

--- Implementierung/Z_Curve.c
+++ Implementierung/Z_Curve.c
@@ -7,8 +7,109 @@
 
 typedef int coord_t;
 
+void z_curve_iterative(unsigned degree, coord_t* x, coord_t* y);
 void z_curve(unsigned degree, coord_t* x, coord_t* y);
 void z_curve_recursive(unsigned degree, coord_t start_x, coord_t start_y, coord_t* x, coord_t* y, unsigned* index);
+
+void z_curve_at(unsigned degree, size_t idx, coord_t* x, coord_t* y);
+size_t z_curve_pos(unsigned degree, coord_t x, coord_t y);
+
+
+
+
+int main(int argc, char *argv[]) {
+    //for testing purpose will be replaced with inputs later
+
+    unsigned testDegree = 4;
+    coord_t testX = 0;
+    coord_t testY = 1;
+    //3rd method test check
+    size_t index = z_curve_pos(testDegree, testX, testY);
+    //printf("3rd Method :Index for (%d, %d): %zu\n", testX, testY, index);
+
+    //calculate number of points based on degree
+    unsigned numberOfPoints = 1 << (2 * testDegree);
+
+    //allocate space of coordinates
+    coord_t* x = (coord_t*)malloc(numberOfPoints * sizeof(coord_t));
+    coord_t* y = (coord_t*)malloc(numberOfPoints * sizeof(coord_t));
+
+    //create image (SVG for now)
+    FILE* svgFile = fopen("zcurve.svg", "wb");
+    if (svgFile == NULL) {
+        printf("Error opening the SVG file.\n");
+        return -1;
+    }
+
+    //clock setup (could prob be done cleaner)
+    clock_t begin, end;
+    float z;
+    begin = clock();
+
+    z_curve(testDegree, x, y);
+
+    //2nd method test check
+    coord_t coordX, coordY;
+    //size_t index =64;
+
+    //z_curve_at(testDegree, index,  &coordX, &coordY);
+    //void z_curve_at(unsigned degree, size_t idx, coord_t* x, coord_t* y)
+
+    // Print the resulting coordinates
+    //printf("2nd Method :Coordinates for index %zu on the Z-curve:\n", index);
+    //printf("x = %d\n", coordX);
+    //printf("y = %d\n", coordY);
+
+    end = clock();
+    z = end - begin;
+    z /= CLOCKS_PER_SEC;
+    if (z != 0) {
+        printf("Runtime: %f seconds\n", z);
+    }
+
+    //to determine the width and height of the SVG Image we need to find min/max of x and y
+    coord_t scalingFactor = 10;
+    coord_t minX = x[0];
+    coord_t minY = y[0];
+    coord_t maxX = x[0];
+    coord_t maxY = y[0];
+    for (unsigned i = 1; i < numberOfPoints; i++) {
+        if (x[i] < minX) minX = x[i];
+        if (x[i] > maxX) maxX = x[i];
+        if (y[i] < minY) minY = y[i];
+        if (y[i] > maxY) maxY = y[i];
+    }
+
+    //dimensions of the SVG image
+    coord_t width = (maxX - minX + 1) * scalingFactor+100;
+    coord_t height = (maxY - minY + 1) * scalingFactor;
+
+    fprintf(svgFile, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\">\n", width, height); //header
+
+    for (unsigned i = 0; i < numberOfPoints; i++) {
+        //adjust the coordinates by subtracting the minimum values
+        coord_t adjustedX1 = (x[i] - minX) * scalingFactor;
+        coord_t adjustedY1 = (y[i] - minY) * scalingFactor;
+
+        //check if there is a next point to draw a line
+        if (i + 1 < numberOfPoints) {
+            coord_t adjustedX2 = (x[i + 1] - minX) * scalingFactor;
+            coord_t adjustedY2 = (y[i + 1] - minY) * scalingFactor;
+
+            fprintf(svgFile, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" />\n",
+                    adjustedX1, adjustedY1, adjustedX2, adjustedY2);
+        }
+    }
+
+    fprintf(svgFile, "</svg>"); //footer
+
+    //free allocated memory
+    free(x);
+    free(y);
+    fclose(svgFile);
+
+    return 0;
+}
 
 void z_curve_iterative(unsigned degree, coord_t* x, coord_t* y) {
   //calculate the total number of points
@@ -147,98 +248,4 @@ size_t z_curve_pos(unsigned degree, coord_t x, coord_t y) {
     free(yCoords);
 
     return index;
-}
-
-int main(int argc, char *argv[]) {
-    //for testing purpose will be replaced with inputs later
-
-    unsigned testDegree = 4;
-    coord_t testX = 0;
-    coord_t testY = 1;
-    //3rd method test check
-    size_t index = z_curve_pos(testDegree, testX, testY);
-    //printf("3rd Method :Index for (%d, %d): %zu\n", testX, testY, index);
-
-    //calculate number of points based on degree
-    unsigned numberOfPoints = 1 << (2 * testDegree);
-
-    //allocate space of coordinates
-    coord_t* x = (coord_t*)malloc(numberOfPoints * sizeof(coord_t));
-    coord_t* y = (coord_t*)malloc(numberOfPoints * sizeof(coord_t));
-
-    //create image (SVG for now)
-    FILE* svgFile = fopen("zcurve.svg", "wb");
-    if (svgFile == NULL) {
-        printf("Error opening the SVG file.\n");
-        return -1;
-    }
-
-    //clock setup (could prob be done cleaner)
-    clock_t begin, end;
-    float z;
-    begin = clock();
-
-    z_curve(testDegree, x, y);
-
-    //2nd method test check
-    coord_t coordX, coordY;
-    //size_t index =64;
-
-    //z_curve_at(testDegree, index,  &coordX, &coordY);
-    //void z_curve_at(unsigned degree, size_t idx, coord_t* x, coord_t* y)
-
-    // Print the resulting coordinates
-    //printf("2nd Method :Coordinates for index %zu on the Z-curve:\n", index);
-    //printf("x = %d\n", coordX);
-    //printf("y = %d\n", coordY);
-
-    end = clock();
-    z = end - begin;
-    z /= CLOCKS_PER_SEC;
-    if (z != 0) {
-        printf("Runtime: %f seconds\n", z);
-    }
-
-    //to determine the width and height of the SVG Image we need to find min/max of x and y
-    coord_t scalingFactor = 10;
-    coord_t minX = x[0];
-    coord_t minY = y[0];
-    coord_t maxX = x[0];
-    coord_t maxY = y[0];
-    for (unsigned i = 1; i < numberOfPoints; i++) {
-        if (x[i] < minX) minX = x[i];
-        if (x[i] > maxX) maxX = x[i];
-        if (y[i] < minY) minY = y[i];
-        if (y[i] > maxY) maxY = y[i];
-    }
-
-    //dimensions of the SVG image
-    coord_t width = (maxX - minX + 1) * scalingFactor+100;
-    coord_t height = (maxY - minY + 1) * scalingFactor;
-
-    fprintf(svgFile, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\">\n", width, height); //header
-
-    for (unsigned i = 0; i < numberOfPoints; i++) {
-        //adjust the coordinates by subtracting the minimum values
-        coord_t adjustedX1 = (x[i] - minX) * scalingFactor;
-        coord_t adjustedY1 = (y[i] - minY) * scalingFactor;
-
-        //check if there is a next point to draw a line
-        if (i + 1 < numberOfPoints) {
-            coord_t adjustedX2 = (x[i + 1] - minX) * scalingFactor;
-            coord_t adjustedY2 = (y[i + 1] - minY) * scalingFactor;
-
-            fprintf(svgFile, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" />\n",
-                    adjustedX1, adjustedY1, adjustedX2, adjustedY2);
-        }
-    }
-
-    fprintf(svgFile, "</svg>"); //footer
-
-    //free allocated memory
-    free(x);
-    free(y);
-    fclose(svgFile);
-
-    return 0;
 }
