317e3faf5860e6288b94178b8e01cd8c30547e57
Pascal <pascal39@gmx.net>
2023-07-11 03:20:04 +0200

    magic alternatives of each of the three methods

--- Implementierung/Z_Curve.c
+++ Implementierung/Z_Curve.c
@@ -5,8 +5,11 @@
 #include <string.h>
 #include <math.h>
 #include <immintrin.h>
+#include <emmintrin.h>
 
 typedef int coord_t;
+static const unsigned int B[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF};
+static const unsigned int S[] = {1, 2, 4, 8};
 
 void z_curve_iterative(unsigned degree, coord_t* x, coord_t* y);
 void z_curve(unsigned degree, coord_t* x, coord_t* y);
@@ -14,6 +17,10 @@ void z_curve_recursive(unsigned degree, coord_t start_x, coord_t start_y, coord_
 
 void z_curve_at(unsigned degree, size_t idx, coord_t* x, coord_t* y);
 size_t z_curve_pos(unsigned degree, coord_t x, coord_t y);
+coord_t ReversePart1By1(coord_t z);
+coord_t Part1By1(coord_t x);
+__m128i ReversePart1By1_simd(__m128i z);
+
 
 void z_curve_at2(unsigned degree, size_t idx, coord_t* x, coord_t* y) {
     //opposite of interleaving similar approach in the z_curve_iterative method (maybe one is faster than the other?)
@@ -39,6 +46,40 @@ void z_curve_at2(unsigned degree, size_t idx, coord_t* x, coord_t* y) {
 
     *x = num1;
     *y = num2;
+}
+
+void z_curve_at_magic(unsigned degree, size_t idx, coord_t* x, coord_t* y) {
+    //needs testing
+    coord_t x1;
+    coord_t y1;
+    y1 = (idx >> 1) & B[0];
+    y1 = (y1 | (y1 >> S[0])) & B[1];
+    y1 = (y1 | (y1 >> S[1])) & B[2];
+    y1 = (y1 | (y1 >> S[2])) & B[3];
+
+    x1 = idx & B[0];
+    x1 = (x1 | (x1 >> S[0])) & B[1];
+    x1 = (x1 | (x1 >> S[1])) & B[2];
+    x1 = (x1 | (x1 >> S[2])) & B[3];
+
+    *x = x1;
+    *y = y1;
+}
+
+inline coord_t ReversePart1By1(coord_t z) {
+    z &= 0x55555555;                  
+    z = (z ^ (z >> 1)) & 0x33333333;  
+    z = (z ^ (z >> 2)) & 0x0f0f0f0f;  
+    z = (z ^ (z >> 4)) & 0x00ff00ff;  
+    z = (z ^ (z >> 8)) & 0x0000ffff;  
+
+    return z;
+}
+
+void z_curve_at_magic2(unsigned degree, size_t idx, coord_t* x, coord_t* y) {
+    //needs testing
+    *x = ReversePart1By1(idx & 0x55555555);
+    *y = ReversePart1By1((idx >> 1) & 0x55555555);
 }
 
 size_t z_curve_pos2(unsigned degree, coord_t x, coord_t y) {
@@ -70,51 +111,83 @@ size_t z_curve_pos3(unsigned degree, coord_t x, coord_t y) {
     return z;
 }
 
-// void z_curve_recursive_simd(unsigned degree, unsigned start_index, unsigned sub_size, coord_t* x, coord_t* y, unsigned* index) {
-//     if (degree == 0) {
-//         unsigned end_index = start_index + sub_size * sub_size;
-//         for (unsigned i = start_index; i < end_index && *index < (1 << (2 * degree)); i++) {
-//             x[*index] = i % sub_size;
-//             y[*index] = i / sub_size;
-//             (*index)++;
-//         }
-//         return;
-//     }
+size_t z_curve_pos_magic(unsigned degree, coord_t x, coord_t y) {
+    //still needs testing
+    x = (x | (x << S[3])) & B[3];
+    x = (x | (x << S[2])) & B[2];
+    x = (x | (x << S[1])) & B[1];
+    x = (x | (x << S[0])) & B[0];
 
-//     unsigned num_points = sub_size * sub_size;
+    y = (y | (y << S[3])) & B[3];
+    y = (y | (y << S[2])) & B[2];
+    y = (y | (y << S[1])) & B[1];
+    y = (y | (y << S[0])) & B[0];
 
-//     __m128i start_index_vec = _mm_set1_epi32(start_index);
-//     __m128i sub_size_vec = _mm_set1_epi32(sub_size);
+    degree = x | (y << 1);
+    return degree;
+}
 
-//     for (unsigned i = 0; i < num_points; i += 4) {
-//         if (*index >= (1 << (2 * degree)))
-//             break;
 
-//         __m128i indices = _mm_add_epi32(start_index_vec, _mm_set_epi32(i + 3, i + 2, i + 1, i));
-//         __m128i x_indices = _mm_and_si128(indices, _mm_set1_epi32(sub_size - 1));
-//         __m128i y_indices = _mm_srli_epi32(indices, 1);
+inline coord_t Part1By1(coord_t x) {
+  x &= 0x0000ffff;                  
+  x = (x ^ (x <<  8)) & 0x00ff00ff; 
+  x = (x ^ (x <<  4)) & 0x0f0f0f0f; 
+  x = (x ^ (x <<  2)) & 0x33333333; 
+  x = (x ^ (x <<  1)) & 0x55555555; 
+  return x;
+}
 
-//         _mm_storeu_si128((__m128i*) &x[*index], x_indices);
-//         _mm_storeu_si128((__m128i*) &y[*index], y_indices);
+size_t z_curve_pos_magic2(unsigned degree, coord_t x, coord_t y) {
+    //needs testing
+    degree = (Part1By1(y) << 1) + Part1By1(x);
+    return degree;
+} 
 
-//         (*index) += 4;
-//     }
+void z_curve_recursive_simd(unsigned degree, unsigned start_index, unsigned sub_size, coord_t* x, coord_t* y, unsigned* index) {
+    if (degree == 0) {
+        unsigned end_index = start_index + sub_size * sub_size;
+        for (unsigned i = start_index; i < end_index && *index < (1 << (2 * degree)); i++) {
+            x[*index] = i % sub_size;
+            y[*index] = i / sub_size;
+            (*index)++;
+        }
+        return;
+    }
 
-//     unsigned next_start_index = start_index;
-//     unsigned next_sub_size = sub_size >> 1;
+    unsigned num_points = sub_size * sub_size;
 
-//     z_curve_recursive_simd(degree - 1, next_start_index, next_sub_size, x, y, index); // Top-left quadrant
-//     z_curve_recursive_simd(degree - 1, next_start_index + next_sub_size, next_sub_size, x, y, index); // Top-right quadrant
-//     z_curve_recursive_simd(degree - 1, next_start_index + next_sub_size * sub_size, next_sub_size, x, y, index); // Bottom-left quadrant
-//     z_curve_recursive_simd(degree - 1, next_start_index + next_sub_size * sub_size + next_sub_size, next_sub_size, x, y, index); // Bottom-right quadrant
-// }
+    __m128i start_index_vec = _mm_set1_epi32(start_index);
+    __m128i sub_size_vec = _mm_set1_epi32(sub_size);
 
-// void z_curve_simd(unsigned degree, coord_t* x, coord_t* y) {
-//     unsigned size = 1 << degree;
-//     unsigned index = 0;
+    for (unsigned i = 0; i < num_points; i += 4) {
+        if (*index >= (1 << (2 * degree)))
+            break;
 
-//     z_curve_recursive_simd(degree, 0, size, x, y, &index);
-// }
+        __m128i indices = _mm_add_epi32(start_index_vec, _mm_set_epi32(i + 3, i + 2, i + 1, i));
+        __m128i x_indices = _mm_and_si128(indices, _mm_set1_epi32(sub_size - 1));
+        __m128i y_indices = _mm_srli_epi32(indices, 1);
+
+        _mm_storeu_si128((__m128i*) &x[*index], x_indices);
+        _mm_storeu_si128((__m128i*) &y[*index], y_indices);
+
+        (*index) += 4;
+    }
+
+    unsigned next_start_index = start_index;
+    unsigned next_sub_size = sub_size >> 1;
+
+    z_curve_recursive_simd(degree - 1, next_start_index, next_sub_size, x, y, index); // Top-left quadrant
+    z_curve_recursive_simd(degree - 1, next_start_index + next_sub_size, next_sub_size, x, y, index); // Top-right quadrant
+    z_curve_recursive_simd(degree - 1, next_start_index + next_sub_size * sub_size, next_sub_size, x, y, index); // Bottom-left quadrant
+    z_curve_recursive_simd(degree - 1, next_start_index + next_sub_size * sub_size + next_sub_size, next_sub_size, x, y, index); // Bottom-right quadrant
+}
+
+void z_curve_simd(unsigned degree, coord_t* x, coord_t* y) {
+    unsigned size = 1 << degree;
+    unsigned index = 0;
+
+    z_curve_recursive_simd(degree, 0, size, x, y, &index);
+}
 
 
 void z_curve_morton(unsigned degree, coord_t* x, coord_t* y) {
@@ -136,6 +209,101 @@ void z_curve_morton(unsigned degree, coord_t* x, coord_t* y) {
         // store the calculated coordinates
         x[i] = x2;
         y[i] = y2;
+    }
+}
+
+
+void z_curve_magic(unsigned degree, coord_t* x, coord_t* y) {
+    unsigned total_points = 1 << (2 * degree);
+    coord_t x1;
+    coord_t y1;
+    for (unsigned i = 0; i < total_points; i ++) {
+        y1 = (i >> 1) & B[0];
+        y1 = (y1 | (y1 >> S[0])) & B[1];
+        y1 = (y1 | (y1 >> S[1])) & B[2];
+        y1 = (y1 | (y1 >> S[2])) & B[3];
+
+        x1 = i & B[0];
+        x1 = (x1 | (x1 >> S[0])) & B[1];
+        x1 = (x1 | (x1 >> S[1])) & B[2];
+        x1 = (x1 | (x1 >> S[2])) & B[3];
+
+        x[i] = x1;
+        y[i] = y1;
+    }
+}
+
+
+void z_curve_magic2(unsigned degree, coord_t* x, coord_t* y) {
+    unsigned total_points = 1 << (2 * degree);
+
+    for (unsigned i = 0; i < total_points; i ++) {
+        x[i] = ReversePart1By1(i & 0x55555555);
+        y[i] = ReversePart1By1((i >> 1) & 0x55555555);
+    }
+}
+
+
+void z_curve_magic_simd(unsigned degree, coord_t* x, coord_t* y) {
+    unsigned total_points = 1 << (2 * degree);
+    const __m128i b0 = _mm_set1_epi32(B[0]);
+    const __m128i b1 = _mm_set1_epi32(B[1]);
+    const __m128i b2 = _mm_set1_epi32(B[2]);
+    const __m128i b3 = _mm_set1_epi32(B[3]);
+    const __m128i s0 = _mm_set1_epi32(S[0]);
+    const __m128i s1 = _mm_set1_epi32(S[1]);
+    const __m128i s2 = _mm_set1_epi32(S[2]);
+
+    for (unsigned i = 0; i < total_points; i += 4) {
+        __m128i indices = _mm_setr_epi32(i, i + 1, i + 2, i + 3);
+
+        __m128i y1 = _mm_srli_epi32(indices, 1) & b0;
+        y1 = (y1 | (y1 >> s0)) & b1;
+        y1 = (y1 | (y1 >> s1)) & b2;
+        y1 = (y1 | (y1 >> s2)) & b3;
+
+        __m128i x1 = indices & b0;
+        x1 = (x1 | (x1 >> s0)) & b1;
+        x1 = (x1 | (x1 >> s1)) & b2;
+        x1 = (x1 | (x1 >> s2)) & b3;
+
+        _mm_storeu_si128((__m128i*)&x[i], x1);
+        _mm_storeu_si128((__m128i*)&y[i], y1);
+    }
+}
+
+inline __m128i ReversePart1By1_simd(__m128i z) {
+    const __m128i mask1 = _mm_set1_epi32(0x55555555);
+    const __m128i mask2 = _mm_set1_epi32(0x33333333);
+    const __m128i mask3 = _mm_set1_epi32(0x0f0f0f0f);
+    const __m128i mask4 = _mm_set1_epi32(0x00ff00ff);
+    const __m128i mask5 = _mm_set1_epi32(0x0000ffff);
+
+    z = _mm_and_si128(z, mask1);
+    z = _mm_xor_si128(z, _mm_srli_epi32(z, 1));
+    z = _mm_and_si128(z, mask2);
+    z = _mm_xor_si128(z, _mm_srli_epi32(z, 2));
+    z = _mm_and_si128(z, mask3);
+    z = _mm_xor_si128(z, _mm_srli_epi32(z, 4));
+    z = _mm_and_si128(z, mask4);
+    z = _mm_xor_si128(z, _mm_srli_epi32(z, 8));
+    z = _mm_and_si128(z, mask5);
+
+    return z;
+}
+
+void z_curve_magic2_simd(unsigned degree, coord_t* x, coord_t* y) { //not faster it seems
+    unsigned total_points = 1 << (2 * degree);
+
+    __m128i indices;
+    for (unsigned i = 0; i < total_points; i += 4) {
+        indices = _mm_setr_epi32(i, i + 1, i + 2, i + 3);
+
+        __m128i x_values = ReversePart1By1_simd(_mm_and_si128(indices, _mm_set1_epi32(0x55555555)));
+        __m128i y_values = ReversePart1By1_simd(_mm_and_si128(_mm_srli_epi32(indices, 1), _mm_set1_epi32(0x55555555)));
+
+        _mm_storeu_si128((__m128i*)&x[i], x_values);
+        _mm_storeu_si128((__m128i*)&y[i], y_values);
     }
 }
 
@@ -209,18 +377,19 @@ int main(int argc, char *argv[]) {
 
     //create image (SVG for now)
 
-    // FILE* svgFile = fopen("zcurve.svg", "wb");
-    // if (svgFile == NULL) {
-    //     printf("Error opening the SVG file.\n");
-    //     return -1;
-    // }
+    FILE* svgFile = fopen("zcurve.svg", "wb");
+    if (svgFile == NULL) {
+        printf("Error opening the SVG file.\n");
+        return -1;
+    }
 
     //clock setup (could prob be done cleaner)
     clock_t begin, end;
     float z;
     begin = clock();
 
-    z_curve_iterative_simd(testDegree, x, y);
+    z_curve_magic2(testDegree, x, y);
+    //z_curve_iterative_simd(testDegree, x, y);
     // z_curve(testDegree, x, y);
     // z_curve_iterative(testDegree, x, y);
     // z_curve_simd(testDegree, x, y);
@@ -246,47 +415,47 @@ int main(int argc, char *argv[]) {
     }
 
 
-    // //to determine the width and height of the SVG Image we need to find min/max of x and y
-    // coord_t scalingFactor = 10;
-    // coord_t minX = x[0];
-    // coord_t minY = y[0];
-    // coord_t maxX = x[0];
-    // coord_t maxY = y[0];
-    // for (unsigned i = 1; i < numberOfPoints; i++) {
-    //     if (x[i] < minX) minX = x[i];
-    //     if (x[i] > maxX) maxX = x[i];
-    //     if (y[i] < minY) minY = y[i];
-    //     if (y[i] > maxY) maxY = y[i];
-    // }
+    //to determine the width and height of the SVG Image we need to find min/max of x and y
+    coord_t scalingFactor = 10;
+    coord_t minX = x[0];
+    coord_t minY = y[0];
+    coord_t maxX = x[0];
+    coord_t maxY = y[0];
+    for (unsigned i = 1; i < numberOfPoints; i++) {
+        if (x[i] < minX) minX = x[i];
+        if (x[i] > maxX) maxX = x[i];
+        if (y[i] < minY) minY = y[i];
+        if (y[i] > maxY) maxY = y[i];
+    }
 
-    // //dimensions of the SVG image
-    // coord_t width = (maxX - minX + 1) * scalingFactor+100;
-    // coord_t height = (maxY - minY + 1) * scalingFactor;
+    //dimensions of the SVG image
+    coord_t width = (maxX - minX + 1) * scalingFactor+100;
+    coord_t height = (maxY - minY + 1) * scalingFactor;
 
-    // fprintf(svgFile, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\">\n", width, height); //header
+    fprintf(svgFile, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%d\" height=\"%d\">\n", width, height); //header
 
-    // for (unsigned i = 0; i < numberOfPoints; i++) {
-    //     //adjust the coordinates by subtracting the minimum values
-    //     coord_t adjustedX1 = (x[i] - minX) * scalingFactor;
-    //     coord_t adjustedY1 = (y[i] - minY) * scalingFactor;
+    for (unsigned i = 0; i < numberOfPoints; i++) {
+        //adjust the coordinates by subtracting the minimum values
+        coord_t adjustedX1 = (x[i] - minX) * scalingFactor;
+        coord_t adjustedY1 = (y[i] - minY) * scalingFactor;
 
-    //     //check if there is a next point to draw a line
-    //     if (i + 1 < numberOfPoints) {
-    //         coord_t adjustedX2 = (x[i + 1] - minX) * scalingFactor;
-    //         coord_t adjustedY2 = (y[i + 1] - minY) * scalingFactor;
+        //check if there is a next point to draw a line
+        if (i + 1 < numberOfPoints) {
+            coord_t adjustedX2 = (x[i + 1] - minX) * scalingFactor;
+            coord_t adjustedY2 = (y[i + 1] - minY) * scalingFactor;
 
-    //         fprintf(svgFile, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" />\n",
-    //                 adjustedX1, adjustedY1, adjustedX2, adjustedY2);
-    //     }
-    // }
+            fprintf(svgFile, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" />\n",
+                    adjustedX1, adjustedY1, adjustedX2, adjustedY2);
+        }
+    }
 
 
-    // fprintf(svgFile, "</svg>"); //footer
+    fprintf(svgFile, "</svg>"); //footer
 
     //free allocated memory
     free(x);
     free(y);
-    // fclose(svgFile);
+    fclose(svgFile);
 
     return 0;
 }
